12c1c9d8e26337a3be19d1fa1d36a9ef769469f3
sarandeep18 <sarandeepjetti83@gmail.com>
2026-09-17 11:31:30 +0530

    Use environment variable for server port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = require('./database');
 const calculateResult = require('./calculateResult');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
